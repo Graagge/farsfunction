87d54b8cea13072059d3f024ebeeb803bf5ebf84
Graagge <72801039+Graagge@users.noreply.github.com>
2020-12-17 17:15:17 +0100

    Made a functioning test

--- tests/testthat.R
+++ tests/testthat.R
@@ -11,11 +11,7 @@ library(maps)
 library(readr)
 library(tidyr)
 
-# global test
-
-test_check(farsfunction)
-
 # make_filename test
 
 test_that("Testing make_filename", {
-  expect_equal(make_filename(2019), "accident_2019.csv.bz2")})
+  expect_equal(farsfunction::make_filename(2019), "accident_2019.csv.bz2")})
